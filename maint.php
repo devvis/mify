5bc7da7b53d9cb5dd1fc41122744b191a74d0a1e
Gustav Eklundh <gustav@xcoders.info>
2014-01-05 18:09:19 +0100

    Added default page when no db is found.

---
+++ maint.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Something is broken.</title>
+	</head>
+	<body>
+		<h1>Emergency maintenance-mode activated.</h1>
+		<h2>Avaiting further instructions from my master.</h2>
+	</body>
+</html>
